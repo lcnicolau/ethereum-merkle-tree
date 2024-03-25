b137367c2ae4f25172b7ac26b3276925e49abf2a
Leonardo Cabrera <lcnicolau@gmx.com>
2024-03-25 17:32:19 -0300

    feat: server verification

--- server/index.js
+++ server/index.js
@@ -6,20 +6,18 @@ const port = 1225;
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+// the server is only allowed to store one 32 byte value in memory, which has to be enough to determine who is on the list,
+// so the merkle root represents the whole nice list
+const MERKLE_ROOT = 'ddd59a2ffccddd60ff47993312821cd57cf30f7f14fb82937ebe2c4dc78375aa';
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const { name, proof } = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  // prove that a name is in the list
+  if (verifyProof(proof, name, MERKLE_ROOT)) {
     res.send("You got a toy robot!");
-  }
-  else {
+  } else {
     res.send("You are not on the list :(");
   }
 });
