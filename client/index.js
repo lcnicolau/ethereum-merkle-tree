032c5d21a3ab4a034661eaf9099cb5f01c84ca37
Leonardo Cabrera <lcnicolau@gmx.com>
2024-03-25 17:35:56 -0300

    feat: client proof

--- client/index.js
+++ client/index.js
@@ -4,14 +4,23 @@ const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
-async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+async function main(name) {
+  // prove to the server we're on the nice list
+  const tree = new MerkleTree(niceList);
+  const index = niceList.indexOf(name);
+  const proof = tree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: name,
+    proof: proof
   });
 
   console.log({ gift });
 }
 
-main();
+const name = process.argv.slice(2).join(" ");
+if (name) {
+  main(name);
+} else {
+  console.error("Please type your name at the end of the command line");
+}
